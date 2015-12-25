151ffeb6406c1d229aee813db209f02da22b0fca
denblo <Denis.Blokhin@transas.com>
2015-12-25 18:59:32 +0300

    some recipes

--- client/crab.js
+++ client/crab.js
@@ -2,28 +2,43 @@ angular.module('crab', ['angular-meteor', 'ui.router']);
 
 angular.module('crab').controller('ItemListCtrl', ['$scope', '$meteor',
   function ($scope, $meteor) {
+    //$scope.$meteorSubscribe('items');
+
+    $meteor.autorun($scope, function () {
+      $meteor.subscribe('items', {
+        query: $scope.getReactively('filter.query')
+      }).then(function () {
+        console.log('after autorun');
+      });
+    });
+
     $scope.items = $meteor.collection(Items);
+
+    $scope.getItemIcon = function (item) {
+      var nameParts = item.name.split(':');
+      var modName = nameParts[0].replace('|', '_');
+      return 'mods/' + modName + '/' + item.id + '_' + item.meta + '.png';
+    };
   }
 ]);
 
 angular.module('crab').controller('ItemCreateFormCtrl', ['$scope', '$meteor',
   function ($scope, $meteor) {
-    $scope.items = $meteor.collection(Items);
+    $scope.$meteorSubscribe('items');
 
-    $scope.create = function (item) {
-      if (item.name !== '') {
-        $scope.items.push(item);
-        $scope.item = {
-          name: ''
-        };
-      }
-    };
+    $scope.create = function (item) {};
   }
 ]);
 
-angular.module('crab').controller('ItemCtrl', ['$scope', '$stateParams',
-  function ($scope, $stateParams) {
-    console.log('in item ctrl', $stateParams);
+angular.module('crab').controller('ItemCtrl', ['$scope', '$stateParams', '$meteor',
+  function ($scope, $stateParams, $meteor) {
+    $meteor.subscribe('recipes', {
+      id: parseInt($stateParams.id, 10),
+    }).then(function () {
+      console.log(arguments);
+    });
+
+    $scope.recipes = $meteor.collection(Recipes);
   }
 ]);
 
@@ -31,17 +46,23 @@ angular.module('crab').config(['$stateProvider', '$urlRouterProvider',
   function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
 
-    console.log(UiRouter);
-
     $stateProvider.state('item', {
-      url: '/item/:itemName',
-      templateUrl: 'client/templates/item.html',
-      controller: 'ItemCtrl',
-    })
-    .state('itemList', {
-      url: '/items',
-      templateUrl: 'client/templates/items.html',
-      controller: 'ItemListCtrl',
-    });
+        url: '/item/:id/:meta',
+        templateUrl: 'client/templates/item.html',
+        controller: 'ItemCtrl',
+      })
+      .state('itemList', {
+        url: '/items',
+        templateUrl: 'client/templates/items.html',
+        controller: 'ItemListCtrl',
+      });
+  }
+]);
+
+angular.module('crab').controller('NavBarCtrl', ['$scope', '$meteor',
+  function ($scope, $meteor) {
+    $scope.doImport = function () {
+      $meteor.call('import', 'fuh');
+    };
   }
 ]);
--- server/crab.js
+++ server/crab.js
@@ -1,3 +1,85 @@
 Meteor.startup(function () {
   console.log('started');
 });
+
+Meteor.methods({
+  import: function () {
+    var path = Npm.require('path');
+
+    //var itemsFile = path.join('../../../../../dumps/dumps/export_item.json');
+
+    //var absoluteFilePath = path.resolve(itemsFile);
+
+    //var items = Npm.require(absoluteFilePath)
+
+    //for (var i = 0; i < items.length; i++) {
+    //Items.insert(items[i]);
+    //}
+
+    //console.log('done', i, 'items');
+
+    var recipesFile = path.join('../../../../../public/crafting_handler/recipes.json');
+
+    var absoluteRecipeFilePath = path.resolve(recipesFile);
+    console.log(absoluteRecipeFilePath);
+
+    var handlers = Npm.require(absoluteRecipeFilePath)
+
+    for (var i = 0; i < handlers.length; i++) {
+      var handler = handlers[i];
+
+      console.log('start', handler.name);
+      for (var j = 0; j < handler.recipes.length; j++) {
+        var recipe = handler.recipes[j];
+        recipe.handlerName = handler.name;
+
+        Recipes.insert(recipe);
+      }
+
+    }
+
+  },
+  //getRecipes: function (output) {
+
+    //var query = {
+      //'result.items.id': output.id
+    //};
+
+    //if (item.meta) {
+      //query['result.items.meta'] = output.meta;
+    //};
+
+    //var recipes = Recipes.find(query).fetch();
+
+  //}
+});
+
+Meteor.publish('items', function (params) {
+  console.log('in publish', params);
+
+  var query = {};
+  if (params.query) {
+    query.displayName = {
+      $regex: new RegExp(params.query, 'i')
+    };
+  }
+
+  return Items.find(query, {
+    limit: 20
+  });
+});
+
+Meteor.publish('recipes', function (item) {
+
+  var query = {
+    'result.items.id': item.id
+  };
+
+  if (item.meta) {
+    query['result.items.meta'] = item.meta;
+  };
+
+  console.log('call query', query);
+
+  return Recipes.find(query);
+});
